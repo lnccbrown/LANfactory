3bf3c1feb651aa56d15cf6455c256e3db0071f5e
Alexander <alexanderfengler@gmx.de>
2025-07-01 21:06:01 +0200

    run ruff

--- src/lanfactory/utils/util_funs.py
+++ src/lanfactory/utils/util_funs.py
@@ -7,9 +7,7 @@ from pathlib import Path
 
 
 # TODO: this is now in ssms. Delete this file and import from ssms instead.
-def try_gen_folder(
-    folder: str | None = None, allow_abs_path_folder_generation: bool = True
-) -> None:
+def try_gen_folder(folder: str | None = None, allow_abs_path_folder_generation: bool = True) -> None:
     """Function to generate a folder from a string. If the folder already exists, it will not be generated.
 
     Arguments
@@ -115,8 +113,6 @@ def save_configs(
     )
     print("Saved network config")
     # Save train config
-    pickle.dump(
-        train_config, open(save_folder + "/" + model_id + "_train_config.pickle", "wb")
-    )
+    pickle.dump(train_config, open(save_folder + "/" + model_id + "_train_config.pickle", "wb"))
     print("Saved train config")
     return
