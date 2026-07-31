9f3170b00fcef26e49bb2c28a5193e5a1d7d8f63
cpaniaguam <cpaniaguam@gmail.com>
2026-07-31 15:48:25 -0400

    Fix unpacking

--- src/lanfactory/trainers/jax_mlp.py
+++ src/lanfactory/trainers/jax_mlp.py
@@ -323,7 +323,7 @@ class ModelTrainerJaxMLP:
 
             if train:
                 grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-                loss, _, grads = grad_fn(state.params)
+                (loss, _), grads = grad_fn(state.params)
                 return grads, loss
             else:
                 loss, _ = loss_fn(state.params)
