fe1d7d64d833e3c1445d46790e9bbefac9cd6af3
Carlos Paniagua <cpaniaguam@gmail.com>
2026-08-03 11:53:57 -0400

    Fix linting

--- src/lanfactory/hf/__init__.py
+++ src/lanfactory/hf/__init__.py
@@ -4,9 +4,6 @@ This module provides utilities for uploading trained models to and
 downloading models from HuggingFace Hub.
 """
 
-DEFAULT_REPO_ID = "franklab/HSSM"
-VALID_NETWORK_TYPES = ("lan", "cpn", "opn")
-
 from lanfactory.hf.download import download_model
 from lanfactory.hf.model_card import (
     ModelCardConfig,
@@ -14,6 +11,9 @@ from lanfactory.hf.model_card import (
     load_model_card_yaml,
 )
 from lanfactory.hf.upload import upload_model
+
+DEFAULT_REPO_ID = "franklab/HSSM"
+VALID_NETWORK_TYPES = ("lan", "cpn", "opn")
 
 __all__ = [
     "DEFAULT_REPO_ID",
